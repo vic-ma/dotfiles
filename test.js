c8cae83e92adea28f56ec020e4e9add56474f021
Victor Ma <victorma649@gmail.com>
2023-11-02 02:49:42 -0400

    sdfsd

--- test.js
+++ test.js
@@ -1,10 +1,14 @@
-console.log("FDSFSDF");
-console.log("FDSFSDF");
-console.log("FDSFSDF");
-console.log("FDSFSDF");
-console.log("FDSFSDF");
-console.log("FDSFSDF");
-console.log("FDSFSDF");
-console.log("FDSFSDF");
-console.log("FDSFSDF");
-console.log("FDSFSDF");
+function updateKofiWidget() {
+  kofiWidgetOverlay.draw('mimid', {
+    'type': 'floating-chat',
+    'floating-chat.donateButton.text': 'Support me',
+    'floating-chat.donateButton.background-color': '#00b9fe',
+    'floating-chat.donateButton.text-color': '#fff'
+  });
+}
+
+// Call the function initially
+updateKofiWidget();
+
+// Set up an interval to call the function every 1 second
+setInterval(updateKofiWidget, 1000);
